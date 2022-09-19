5503f272e3e1102cc8adf15e05d8e5b908c0b3f5
Junaid Malik <junmalik109@gmail.com>
2022-09-19 18:55:31 +0100

    location selection jsx layout

--- components/LocationSelector.js
+++ components/LocationSelector.js
@@ -6,16 +6,44 @@ const style = {
     inputBoxes: 'flex flex-col mb-4 relative',
     inputBox: 'h-10 mx-4 border-2 bg-[#eeeeee]',
     focusedInputBox: 'border-black',
-    input: 'my-2 rounded-2 p-2 outline-none border-none bg-transparent h-full w-full'
+    svgContainer: 'mx-1',
+    input: 'my-2 rounded-2 p-2 outline-none border-none bg-transparent h-full w-full',
+    verticalLine: 'w-0 h-[2rem] border-black border absolute z-10 left-[2.3rem]'
 }
 const LocationSelector = () => {
-    const [inFocus, setInFocus] = useState('')
+  const [inFocus, setInFocus] = useState('from')
+  const [pickUp, setPickUp] = useState('')
+  const [dropOff, setDropOff] = useState('')
 
   return (
     <div className={style.wrapper}>
     <div className={style.searchHeader}> 
         <div className={style.wrapper}>
             {inFocus === 'from' ? 'Where can we you pick you up?' : 'Where can we drop you off?'}
+        </div>
+        <div className={style.inputBoxes} >
+          <div className={`${style.inputBox} ${
+            inFocus === 'from' && style.focusedInputBox
+            }`}
+            >
+            <div className={style.svgContainer}>
+              <svg viewBox='0 0 24 24' width='1em' height='1em'>
+                <path
+                fillRule='evenodd'
+                clipRule='evenodd'
+                d='M12 14a2 2 0 100-4 2 2 0 000 4zm5-2a5 5 0 11-10 0 5 5 0 0110 0z'>
+                </path>
+              </svg>
+            </div>
+            <input 
+            className={style.input} 
+            placeholder="Enter pickup location"
+            value={pickUp}
+            onChange={(e) => setPickUp(e.target.value)}
+            onFocus={() => setInFocus('from')}
+            >
+            </input>
+          </div>
         </div>
     </div></div>
   )
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const style = {
   fontSize: '2rem',
   main: 'h-full w-screen flex-1 x-10',
   mapContainer: 'flex1-1 h-full w-full',
-  riderRequestContainer: 'h-full w-[400px] m1-[1rem] py-[3rem] absolute left-0 top-0 flex flex-col justify-end', 
+  riderRequestContainer: 'h-full w-[400px] m1-[1rem] py-[3rem] absolute left-0 top-0 flex flex-col justify-end z-20', 
   rideRequest: 'h-full max-h-[700px] bg-white rounded-[1rem] shadow-lg flex flex-col overflow-y-auto',
 }
 
