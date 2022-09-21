28ca23edaf319dcc1dec723cbb607a9cef0923c4
Junaid Malik <junmalik109@gmail.com>
2022-09-21 17:51:54 +0100

    reimport of source files

--- components/Map.js
+++ components/Map.js
@@ -1,6 +1,6 @@
 import { useEffect, useContext } from 'react'
 import mapboxgl from 'mapbox-gl'
-import { HalaContext } from '../context/halaContext'
+import { UberContext } from '../context/uberContext'
 
 const style = {
   wrapper: `flex-1 h-full w-full`,
@@ -9,7 +9,7 @@ const style = {
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
 const Map = () => {
-  const { pickupCoordinates, dropoffCoordinates } = useContext(HalaContext)
+  const { pickupCoordinates, dropoffCoordinates } = useContext(UberContext)
 
   useEffect(() => {
     const map = new mapboxgl.Map({
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import '../styles/globals.css'
 import 'mapbox-gl/dist/mapbox-gl.css'
-import { HalaProvider } from '../context/halaContext'
+import { UberProvider } from '../context/uberContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <HalaProvider>
+    <UberProvider>
       <Component {...pageProps} />
-    </HalaProvider>
+    </UberProvider>
   )
 }
 
