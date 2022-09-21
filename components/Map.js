2f35850c82eeed0f2d5188d0132e7fe306ab44dc
Junaid Malik <junmalik109@gmail.com>
2022-09-21 17:02:12 +0100

    updated map component

--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,44 @@
-import React, { useEffect } from 'react'
+import { useEffect, useContext } from 'react'
 import mapboxgl from 'mapbox-gl'
+import { HalaContext } from '../context/halaContext'
 
 const style = {
-    wrapper: 'h-screen flex flex-col justify-center items-center',
+  wrapper: `flex-1 h-full w-full`,
 }
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
 const Map = () => {
+  const { pickupCoordinates, dropoffCoordinates } = useContext(HalaContext)
 
-    useEffect(() => {
-       
+  useEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/drakosi/ckvcwq3rwdw4314o3i2ho8tph',
+      center: [-99.29011, 39.39172],
+      zoom: 3,
+    })
 
+    if (pickupCoordinates) {
+      addToMap(map, pickupCoordinates)
+    }
 
-    }, [])
-  return (
-    <div className={style.wrapper}>Map</div>
-  )
+    if (dropoffCoordinates) {
+      addToMap(map, dropoffCoordinates)
+    }
+
+    if (pickupCoordinates && dropoffCoordinates) {
+      map.fitBounds([dropoffCoordinates, pickupCoordinates], {
+        padding: 400,
+      })
+    }
+  }, [pickupCoordinates, dropoffCoordinates])
+
+  const addToMap = (map, coordinates) => {
+    const marker1 = new mapboxgl.Marker().setLngLat(coordinates).addTo(map)
+  }
+
+  return <div className={style.wrapper} id='map' />
 }
 
 export default Map
